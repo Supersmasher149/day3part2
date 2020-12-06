c4e436f152eb373bd346aee42dc897e8abf585a8
Supersmasher149 <walter.gibbons@gmail.com>
2020-12-06 17:05:50 -0500

    adding new methoid

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ namespace New_folder__3_
 {
     class Program
     {
+        string[] input = System.IO.File.ReadAllLines("input.txt");
+        static void Encounter(string[] input, int ymove, int xmove)
+        {
+            int x = 0;
+            int hit = 0;
+            for (int i = ymove; i < input.Length; i = i + ymove)
+            {
+                if ()
+            }
+        }
         static void Main()
         {
             string[] input = System.IO.File.ReadAllLines("input.txt");
