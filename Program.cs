a64bc1fcbe60de2649ccb0753da887cc09f93c65
Supersmasher149 <walter.gibbons@gmail.com>
2020-12-07 00:18:03 -0500

    Solved Day 3 part 2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace New_folder__3_
 {
     class Program
     {
-        public static void Encounter(int xmove, int ymove)
+        public static int Encounter(int xmove, int ymove)
         {
             string[] input = System.IO.File.ReadAllLines("input.txt");
             //Console.WriteLine(input[0]);
@@ -29,7 +29,7 @@ namespace New_folder__3_
                 }
                 //Console.WriteLine(input[i]);
             }
-            Console.WriteLine(hit);
+            return hit;
         }
                static void Main()
         {
@@ -58,8 +58,8 @@ namespace New_folder__3_
             }
             //Console.WriteLine(hit);
             //New_folder__3_.Part2.Test();
-            //New_folder__3_.Program.Encounter();
-            New_folder__3_.Program.Encounter(3, 1);
+            //New_folder__3_.Program.Encounter(3, 1);
+            Console.WriteLine(Encounter(1, 1) * Encounter(3, 1) * Encounter(5, 1) * Encounter(7, 1) * Encounter(1, 2));
         }
     }
 }
