610d5a5b70bc9d8b3c8c0b3ed907e491499a1ac5
Supersmasher149 <walter.gibbons@gmail.com>
2020-12-07 18:10:22 -0500

    changes

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ namespace New_folder__3_
 {
     class Program
     {
-        public static int Encounter(int xmove, int ymove)
+        string[] lines = System.IO.File.ReadAllLines("input.txt");
+        public static int Encounter(string[] input, int xmove, int ymove)
         {
-            string[] input = System.IO.File.ReadAllLines("input.txt");
+            //string[] input = System.IO.File.ReadAllLines("input.txt");
             //Console.WriteLine(input[0]);
             //int ymove = 2;
             //int xmove = 1;
@@ -59,7 +60,8 @@ namespace New_folder__3_
             //Console.WriteLine(hit);
             //New_folder__3_.Part2.Test();
             //New_folder__3_.Program.Encounter(3, 1);
-            Console.WriteLine(Encounter(1, 1) * Encounter(3, 1) * Encounter(5, 1) * Encounter(7, 1) * Encounter(1, 2));
+            Console.WriteLine(Encounter(input, 1, 1) * Encounter(input, 3, 1) * Encounter(input, 5, 1) * Encounter(input, 7, 1) * Encounter(input, 1, 2));
+            //Console.WriteLine(Encounter(input, 3, 1));
         }
     }
 }
